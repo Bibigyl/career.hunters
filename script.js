2cbd4a83cc953e4831cc4e953f99474679b5c716
Kate <ekaterina.kryakushina@yandex.ru>
2020-04-17 14:47:13 +0300

    Update

--- script.js
+++ script.js
@@ -1,58 +1,119 @@
 $('document').ready(function () {
-  // header
-  document.onscroll = function () {
-    let menu = document.querySelector('.header');
+    // header
+    document.onscroll = function () {
+        let menu = document.querySelector('.header');
 
-    if (window.scrollY > 150 && !menu.classList.contains('header_slim')) {
-      menu.classList.add('header_slim');
-    }
+        if (window.scrollY > 150 && !menu.classList.contains('header_slim')) {
+            menu.classList.add('header_slim');
+        }
 
-    if (window.scrollY <= 150 && menu.classList.contains('header_slim')) {
-      menu.classList.remove('header_slim');
-    }
-  };
-
-
-  // services cards
-  new Waypoint({
-    element: document.getElementById('work'),
-    offset: 700,
-    handler: function(direction) {
-
-      const $workCards = $('#work .work__item');
-
-      for (let i = 0; i < $workCards.length; i++ ) {
-        setTimeout(() => {
-          $workCards.eq(i).addClass('animated fadeInUp');
-        }, 200*i)
-      }
-    
-    }
-  });
-
-  // review cards
-  new Waypoint({
-    element: document.getElementById('reviews'),
-    offset: 700,
-    handler: function(direction) {
-
-      const $revCards = $('#reviews .rev');
-
-      for (let i = 0; i < $revCards.length; i++ ) {
-        setTimeout(() => {
-          $revCards.eq(i).addClass('animated zoomIn opa1');
-        }, 300*i)
-      }
-    
-    }
-  });
+        if (window.scrollY <= 150 && menu.classList.contains('header_slim')) {
+            menu.classList.remove('header_slim');
+        }
+    };
 
 
+    // button order
+    $('.button_order').on('click', () => {
+        $('a[href="#order"]').trigger('click');
+    })
+
+
+    // scroll
+    jQuery(window).scroll(function () {
+        var $sections = $('section');
+        $sections.each(function (i, el) {
+            var top = $(el).offset().top - 100;
+            var bottom = top + $(el).height();
+            var scroll = $(window).scrollTop();
+            var id = $(el).attr('id');
+            if (scroll > top && scroll < bottom) {
+                $('a.active').removeClass('active');
+                $('a[href="#' + id + '"]').addClass('active');
+            }
+        });
+    });
+
+    $('nav').on('click', 'a', function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top - 83;
+        $('body,html').animate({ scrollTop: top }, 600);
+    });
+
+
+    // services cards
+    new Waypoint({
+        element: document.getElementById('work'),
+        offset: 500,
+        handler: function (direction) {
+            const $workCards = $('#work .work__item');
+
+            for (let i = 0; i < $workCards.length; i++) {
+                setTimeout(() => {
+                    $workCards.eq(i).addClass('animated zoomIn visible');
+                }, 200 * i);
+            }
+        },
+    });
+
+
+    // review cards
+    new Waypoint({
+        element: document.getElementById('reviews'),
+        offset: 700,
+        handler: function (direction) {
+            const $revCards = $('#reviews .rev');
+
+            for (let i = 0; i < $revCards.length; i++) {
+                setTimeout(() => {
+                    $revCards.eq(i).addClass('animated zoomIn visible');
+                }, 300 * i);
+            }
+        },
+    });
+
+    // individual
+    new Waypoint({
+        element: document.getElementById('ind'),
+        offset: 600,
+        handler: function (direction) {
+            $('#ind .ind__img').addClass('animated fadeInLeft fast visible');
+        },
+    });
+
+    new Waypoint({
+        element: document.getElementById('ind'),
+        offset: 600,
+        handler: function (direction) {
+            $('#ind .ind__content').addClass('animated fadeInRight fast visible');
+        },
+    });
+
+    // Us
+    new Waypoint({
+        element: document.getElementById('us'),
+        offset: 600,
+        handler: function (direction) {
+            $('#us .us__img').addClass('animated fadeInRight fast visible');
+        },
+    });
+
+    new Waypoint({
+        element: document.getElementById('us'),
+        offset: 600,
+        handler: function (direction) {
+            $('#us .us__content').addClass('animated fadeInLeft fast visible');
+        },
+    });
+
+    // copyright
+    document.getElementById("yearNow").innerHTML = new Date().getFullYear();
+
+
+    // burger
+    $(".hamburger").click(function(){
+        $(this).toggleClass("is-active");
+        $('#menu').toggleClass('menu_open');
+    });
 });
-
-
-
-
-
-
-
